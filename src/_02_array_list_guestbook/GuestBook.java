268a24edfa42df73a9592048dc5a6ad1298cce38
league <league@imac-34.attlocal.net>
2022-06-24 19:56:22 -0700

    Started on Guest Book. I need to find out how to add arralist values to label

--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -1,6 +1,16 @@
 package _02_array_list_guestbook;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class GuestBook implements ActionListener{
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +19,29 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	
+	JFrame frame = new JFrame("Guest Book");
+	JPanel panel = new JPanel();
+	JButton addNamesButton = new JButton("Add Name");
+	JButton viewNamesButton = new JButton("View Names");
+	JTextField txtInput = new JTextField(10);
+	JLabel label = new JLabel();
+	ArrayList<String> arrList = new ArrayList<String>();
+	
+	public void run() {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton buttonPressed = (JButton) e.getSource();
+		
+		if(buttonPressed == addNamesButton ) {
+			label.add(txtInput);
+		} else if (buttonPressed == viewNamesButton) {
+			label.add(arrList.iterator());
+		}
+		
+	}
+	
 }
